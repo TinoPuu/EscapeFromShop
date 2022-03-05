897ae05dbb085287b6ee04724441d62dea44e608
Attoss <atte.tossavainen@edu.savonia.fi>
2022-03-05 10:43:16 +0200

    MainMenu lisätty
    MainMenu laitettu vaihtamaan Scene, toimivat play ja quit napit

---
+++ EFS/Assets/Script/ButtonsController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit! (Toimii vaan kun buildaa pelin!)");
+        Application.Quit();
+    }
+}
